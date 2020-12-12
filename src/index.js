ceec551b8d14aa80c0f3a08b8ad3e3eb7e541792
Avesh Shaikh <31967343+AveshShaikh0408@users.noreply.github.com>
2020-12-12 16:23:32 +0530

    Reformatting-Structure Merging code from more complex structure to simple and understandable format.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import Greeting from "./Greeting";
 
-ReactDOM.render(
-  <div>
-    <App />
-    <Greeting />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
