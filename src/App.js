dbf8e758dce626139367e6bbe707ca2a173680e0
Avesh Shaikh <31967343+AveshShaikh0408@users.noreply.github.com>
2020-12-12 16:45:46 +0530

    Added-Import-Export-Chapter Default Export and Other exports.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./styles.css";
 
 import Greeting from "./Greeting";
+import PI, { doublePi } from "./IE.js";
 
 export default function App() {
-  return <Greeting />;
+  return (
+    <div>
+      <Greeting />
+      <h1>{PI}</h1> {/* Default Export - which is Pi*/}
+      <h1>{doublePi()}</h1> {/* Other Export - which is DoublePi*/}
+    </div>
+  );
 }
---
+++ src/IE.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const pi = 3.14;
+
+function doublePi() {
+  return pi * 2;
+}
+
+export default pi;
+export { doublePi };
